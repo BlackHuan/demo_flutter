bc9e5a9a909abc6b12d4b3272326321a6c49cefc
BlackHuan <black_huan@foxmail.com>
2024-01-10 08:50:57 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        body: Center(
+          // child: Text('Hello World1!'),
+          child: ElevatedButton(onPressed: () {
+
+          },
+          child: const Text('Elevated')
+        ),
+        )
+        ),
+    );
+  }
+}
