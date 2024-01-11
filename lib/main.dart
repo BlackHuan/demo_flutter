a0080f470228b7446bca027c8b31d0482fbdfb59
BlackHuan <black_huan@foxmail.com>
2024-01-12 07:35:33 +0800

    dev: call command tool

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:process/process.dart';
 
 void main() {
   runApp(const MainApp());
@@ -22,54 +23,73 @@ class MainApp extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                  onPressed: () async {
-                    FilePickerResult? result =
-                        await FilePicker.platform.pickFiles();
-              
-                    if (result != null) {
-                      File file = File(result.files.single.path!);
-              
-                      Stream<String> lines = file.openRead().transform(utf8.decoder).transform(const LineSplitter());
-                      try {
-                        await for (var line in lines) {
-                          print('$line: ${line.length} characters');
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        FilePickerResult? result =
+                            await FilePicker.platform.pickFiles();
+
+                        if (result != null) {
+                          File file = File(result.files.single.path!);
+
+                          Stream<String> lines = file
+                              .openRead()
+                              .transform(utf8.decoder)
+                              .transform(const LineSplitter());
+                          try {
+                            await for (var line in lines) {
+                              print('$line: ${line.length} characters');
+                            }
+                            print('File is now closed.');
+                          } catch (e) {
+                            print('Error: $e');
+                          }
+                        } else {
+                          // User canceled the picker
                         }
-                        print('File is now closed.');
-                      } catch (e) {
-                        print('Error: $e');
-                      }
-                    } else {
-                      // User canceled the picker
-                    }
-                  },
-                  child: const Text('选择文件')),
+                      },
+                      child: const Text('读取文件')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        // Find the correct local path: https://docs.flutter.dev/cookbook/persistence/reading-writing-files#1-find-the-correct-local-path
+
+                        String? outputFile = await FilePicker.platform.saveFile(
+                          dialogTitle: 'Please select an output file:',
+                          fileName: 'output-file.pdf',
+                        );
+
+                        if (outputFile == null) {
+                          // User canceled the picker
+                        } else {
+                          var file = File(outputFile);
+                          var sink = file.openWrite();
+                          sink.write('FILE ACCESSED ${DateTime.now()}\n');
+
+                          // Close the IOSink to free system resources.
+                          sink.close();
+                        }
+                      },
+                      child: const Text('保存文件')),
+                ),
+              ],
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                  onPressed: () async {
-                    // Find the correct local path: https://docs.flutter.dev/cookbook/persistence/reading-writing-files#1-find-the-correct-local-path
-
-                    String? outputFile = await FilePicker.platform.saveFile(
-                      dialogTitle: 'Please select an output file:',
-                      fileName: 'output-file.pdf',
-                    );
-
-                    if (outputFile == null) {
-                      // User canceled the picker
-                    } else {
-                      var file = File(outputFile);
-                      var sink = file.openWrite();
-                      sink.write('FILE ACCESSED ${DateTime.now()}\n');
-
-                      // Close the IOSink to free system resources.
-                      sink.close();
+              child:
+                  ElevatedButton(onPressed: () async {
+                    var p = ProcessWrapper(await Process.start('ls', ['-l']));
+                    print(p.pid);
+                    await for (var line in p.stdout.transform(utf8.decoder)
+                              .transform(const LineSplitter())) {
+                      print(line);
                     }
-                  },
-                  child: const Text('保存文件')),
+                  }, child: const Text('调用命令行')),
             ),
           ],
         ),
